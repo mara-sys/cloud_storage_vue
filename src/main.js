0a7c67031c577ed28a21cd5e1731218e6e5b7423
mara-sys <1528681067@qq.com>
2025-07-14 11:27:49 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+app.use(ElementPlus)
+createApp(App).mount('#app')
